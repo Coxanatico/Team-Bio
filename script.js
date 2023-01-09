ca76af5b0fb21dd0f315e525a381c41ed13eefe9
Coxanatico <91600980+Coxanatico@users.noreply.github.com>
2023-01-09 19:52:31 -0300

    preliminar version

---
+++ script.js
@@ -0,0 +1,156 @@
+const persons = [
+  {
+    name: "Enrico Merighi",
+    photo: "https://vectorified.com/images/missing-person-icon-12.png",
+    title: "Head of PDP",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      facebook: "#",
+      twitter: "https://twitter.com/knyttneve",
+      linkedin: "#"
+    }
+  },
+  {
+    name: "David Newman",
+    photo: "https://vectorified.com/images/missing-person-icon-12.png",
+    title: "Crop Harvester",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      facebook: "#",
+      twitter: "https://twitter.com/knyttneve",
+      linkedin: "#"
+    }
+  },
+  {
+    name: "Michael Frantz",
+    photo: "https://vectorified.com/images/missing-person-icon-12.png",
+    title: "Crop Production",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      facebook: "#",
+      twitter: "https://twitter.com/knyttneve",
+      linkedin: "#"
+    }
+  },
+  {
+    name: "Michal Epting",
+    photo: "https://vectorified.com/images/missing-person-icon-12.png",
+    title: "Large & Compact Tractors",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      facebook: "#",
+      twitter: "https://twitter.com/knyttneve",
+      linkedin: "#"
+    }
+  },
+  {
+    name: "Madhur Garg",
+    photo: "https://vectorified.com/images/missing-person-icon-12.png",
+    title: "Powertrain & Technology",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      facebook: "#",
+      twitter: "https://twitter.com/knyttneve",
+      linkedin: "#"
+    }
+  },
+  {
+    name: "Sachin Raodeo",
+    photo: "https://vectorified.com/images/missing-person-icon-12.png",
+    title: "APAC",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      facebook: "#",
+      twitter: "https://twitter.com/knyttneve",
+      linkedin: "#"
+    }
+  },
+  {
+    name: "Guilherme Barini",
+    photo: "https://vectorified.com/images/missing-person-icon-12.png",
+    title: "LATAM",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      facebook: "#",
+      twitter: "https://twitter.com/knyttneve",
+      linkedin: "#"
+    }
+  },
+];
+
+const app = new Vue({
+  el: "#app",
+  data() {
+    return {
+      persons: persons,
+      selectedPersonIndex: null,
+      isSelected: false,
+      selectedPerson: null,
+      inlineStyles: null,
+      isReady: false,
+      isOk: false,
+      selectedPersonData: {
+        name: null,
+        title: null,
+        photo: null,
+        social: {
+          facebook: null,
+          twitter: null,
+          linkedin: null
+        }
+      }
+    };
+  },
+  methods: {
+    selectPerson(index, el) {
+      if (!this.isOk) {
+        this.selectedPersonIndex = index;
+        this.isSelected = true;
+        el.target.parentElement.className == "person-details"
+          ? (this.selectedPerson = el.target.parentElement.parentElement)
+          : (this.selectedPerson = el.target.parentElement);
+
+        this.selectedPerson.classList.add("person-selected");
+        this.selectedPerson.setAttribute(
+          "style",
+          `width:${this.selectedPerson.offsetWidth}px;`
+        );
+        this.selectedPersonData = this.persons[index];
+        window.setTimeout(() => {
+          this.inlineStyles = `width:${this.selectedPerson
+            .offsetWidth}px;height:${this.selectedPerson
+            .offsetHeight}px;left:${this.selectedPerson.offsetLeft}px;top:${this
+            .selectedPerson.offsetTop}px;position:fixed`;
+          this.selectedPerson.setAttribute("style", this.inlineStyles);
+        }, 400);
+        window.setTimeout(() => {
+          this.isReady = true;
+          this.isOk = true;
+        }, 420);
+      } else {
+        this.reset();
+      }
+    },
+    reset() {
+      this.isReady = false;
+      window.setTimeout(() => {
+        this.selectedPerson.classList.add("person-back");
+      }, 280);
+      window.setTimeout(() => {
+        this.selectedPerson.setAttribute("style", "");
+      }, 340);
+      window.setTimeout(() => {
+        this.isSelected = false;
+        this.selectedPerson.classList.remove("person-back", "person-selected");
+        this.isOk = false;
+      }, 400);
+    }
+  }
+});
