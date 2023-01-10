2c755b95eed4bc762ec2c483301a377a63ca2a61
Coxanatico <91600980+Coxanatico@users.noreply.github.com>
2023-01-10 13:23:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const persons = [
   {
     name: "Enrico Merighi",
-    photo: "https://vectorified.com/images/missing-person-icon-12.png",
+    photo: "./picutes/E. Merighi.jfif",
     title: "Head of PDP",
     bio:
       "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
@@ -13,7 +13,7 @@ const persons = [
   },
   {
     name: "David Newman",
-    photo: "https://vectorified.com/images/missing-person-icon-12.png",
+    photo: "./picutes/D. Newman.jfif",
     title: "Crop Harvester",
     bio:
       "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
@@ -49,7 +49,7 @@ const persons = [
   },
   {
     name: "Madhur Garg",
-    photo: "https://vectorified.com/images/missing-person-icon-12.png",
+    photo: "./picutes/M. Garg.jfif",
     title: "Powertrain & Technology",
     bio:
       "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
@@ -61,7 +61,7 @@ const persons = [
   },
   {
     name: "Sachin Raodeo",
-    photo: "https://vectorified.com/images/missing-person-icon-12.png",
+    photo: "./picutes/S. Raodeo.jfif",
     title: "APAC",
     bio:
       "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
@@ -73,7 +73,7 @@ const persons = [
   },
   {
     name: "Guilherme Barini",
-    photo: "https://vectorified.com/images/missing-person-icon-12.png",
+    photo: "./picutes/G. Barini.jfif.png",
     title: "LATAM",
     bio:
       "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
